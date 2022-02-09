e1cd5db928248ab23f5f8a583d796cd8a289b4f4
Roman Kosarev <rmksrv@outlook.com>
2022-02-09 18:14:38 +0300

    implemented docx parser

---
+++ core/constants.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Days(Enum):
+    Monday = "ПОНЕДЕЛЬНИК"
+    Tuesday = "ВТОРНИК"
+    Wednesday = "СРЕДА"
+    Thursday = "ЧЕТВЕРГ"
+    Friday = "ПЯТНИЦА"
+    Saturday = "СУББОТА"
